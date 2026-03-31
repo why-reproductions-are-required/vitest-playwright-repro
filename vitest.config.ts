a29895e91aa9ee33e18ec5bf0ed7bb2c5e2b8792
MK <fengmk2@gmail.com>
2026-03-31 13:55:37 +0800

    Vite+ migrate test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "vitest/config";
-import { playwright } from "@vitest/browser-playwright";
+import { defineConfig } from "vite-plus";
+import { playwright } from "vite-plus/test/browser-playwright";
 
 export default defineConfig({
-	test: {
-		browser: {
-			enabled: true,
-			provider: playwright(),
-			instances: [{ browser: "chromium" }],
-		}
-	}
+  test: {
+    browser: {
+      enabled: true,
+      provider: playwright(),
+      instances: [{ browser: "chromium" }],
+    },
+  },
 });
